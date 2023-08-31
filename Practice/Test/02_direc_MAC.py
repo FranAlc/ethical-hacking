9fc57cf1fd087267ff38fc7b4d399886d45cdff8
FranAlc <fgd.galan@gmail.com>
2023-08-31 15:17:25 -0300

    Cambiar direccion MAC

--- Practice/Test/02_direc_MAC.py
+++ Practice/Test/02_direc_MAC.py
@@ -1,5 +1,6 @@
 #Programa que cambia la direccion MAC
 import subprocess
+import optparse
 
 #1
 interface = "eth0"
@@ -11,3 +12,34 @@ subprocess.call(f"ifconfig {interface}  down",shell=True)
 subprocess.call(f"ifconfig {interface}  hw ether  {new_mac}",shell=True)
 subprocess.call(f"ifconfig {interface}  up",shell=True)
 
+#2
+interface = input("interface > ")
+new_mac = input("mac > ")
+
+print(f"[+] Se cambio con exito la direccion MAC de la interfaz {interface} a {new_mac}")
+
+subprocess.call(f"ifconfig {interface} down", shell= True)
+subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
+subprocess.call(f"ifconfig {interface} up", shell=True)
+
+#3
+#Este programa que realice esta mejor estructurado
+def obtener_argumentos():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface", dest= "interface", help= "Interface para cambiar direccion MAC")
+    parser.add_option("-m","--mac", dest= "new_mac", help= "Nueva direccion MAC")
+    return parser.parse_args()
+
+
+def cambiar_mac(interface,new_mac):
+    print(f"[+] Se cambio con exito la direccion MAC de la interfaz {interface} a {new_mac}")
+
+    subprocess.call(["ifconfig", interface , "down"])
+    subprocess.call(["ifconfig", interface , "hw", "ether" , new_mac])
+    subprocess.call(["ifconfig", interface , "up"])
+
+
+#variables globales
+
+(options,arguments) = obtener_argumentos()
+cambiar_mac(options.interface,options.new_mac)
