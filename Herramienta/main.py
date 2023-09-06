091e88a6d623383397552ecd0a9e50e55c8a53ad
FranAlc <fgd.galan@gmail.com>
2023-09-06 05:32:40 -0300

    menu

--- Herramienta/main.py
+++ Herramienta/main.py
@@ -20,4 +20,19 @@ print("[03] Exploit PC Windows")
 print("[04] Extraccion de informacion")
 print("[05] Exit")
 
+opcion= input("> ")
+while True:
+    if opcion == '01':
+        print("Red / Redes\n") 
+    if opcion == '02':
+        print("Exploit PC Linux\n") 
+    if opcion == '03':
+        print("Exploit PC Windows\n") 
+    if opcion == '04':
+        print("Extraccion de información.") 
+    if opcion == '05':
+        print("Saliste.\n") 
+        break
+
+    
 
