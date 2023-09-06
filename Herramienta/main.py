82af8ada64f5248d5e56966affb05f8364606014
FranAlc <fgd.galan@gmail.com>
2023-09-05 21:34:55 -0300

    Organizando Menu

--- Herramienta/main.py
+++ Herramienta/main.py
@@ -10,12 +10,14 @@ def cubo():
 print("*****************************************")
 cubo()
 print("Herramienta:")
-print("")
+print("Escaneo - Exploit")
 print("Coded by Franco Galan")
 print("*****************************************")
 
 print("[01] Red/Redes")
-print("[02] DOS")
-print("[03] Exploit PC Linux")
-print("[04] Exploit PC Windows")
-print("[05] Extraccion")
+print("[02] Exploit PC Linux")
+print("[03] Exploit PC Windows")
+print("[04] Extraccion de informacion")
+print("[05] Exit")
+
+
