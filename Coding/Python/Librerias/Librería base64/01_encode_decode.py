4b96868ef07e5edc79ef0374d35dc0d629d3827a
Franco Galan <99420835+FranAlc@users.noreply.github.com>
2023-10-04 23:00:25 -0300

    base64, codificación y decodificación

---
+++ Coding/Python/Librerias/Librería base64/01_encode_decode.py
@@ -0,0 +1,18 @@
+#encode
+import base64
+
+shDecodificado = b'Texto que sera codificado'
+
+encode = base64.b64encode(shDecodificado)
+
+print(encode)
+# >> VGV4dG8gcXVlIHNlcmEgY29kaWZpY2Fkbw==
+
+
+#decode
+
+shCodificado = b'VGV4dG8gcXVlIHNlcmEgZGVjb2RpZmljYWRv' 
+
+decode = base64.b64decode(shCodificado)
+
+print(decode)
